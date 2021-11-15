b8a00a420a380027cce217affc4b515ca68d2d4c
Madhan <smadhan@clustrex.com>
2021-11-15 13:08:58 +0530

    testing

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ http.createServer(function (req, res) {
 }).listen(5000); //the server object listens on port 8080   
   
 // Console will print the message  
-console.log('Server running at 5000 port via code build and ec2'); 
+console.log('Server running at 5000 port via code build'); 
