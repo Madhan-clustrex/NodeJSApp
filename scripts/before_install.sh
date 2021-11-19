fe7e5ecc5fec86229946a98964c42bdef2515c63
madhan <smadhan@clustrex.com>
2021-11-19 17:01:29 +0530

    testing

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
-
+# sudo yum install python-software-properties -y
+# # sudo apt-add-repository ppa:chris-lea/node.js -y
+# sudo yum update
+# sudo yum install nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
 # Install nodemon
 # sudo npm install nodemon -g
 
 # Install forever module 
 # https://www.npmjs.com/package/forever
-sudo npm install forever -g
+# sudo npm install forever -g
 
 # Clean working folder
 # sudo find /home/ec2-user/test -type f -delete
