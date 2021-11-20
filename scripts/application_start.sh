b14394e26a50205acf521b94bef491315f79a659
madhan <smadhan@clustrex.com>
2021-11-20 12:23:45 +0530

    testing

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,7 +3,7 @@
 # Stop all servers and start the server as a daemon
 # forever stopall
 # forever start /home/ec2-user/nodejsapp/app.js
-cd /home/ec2-user/nodejsapp
 sudo npm install
 sudo npm i -g pm2
+cd /home/ec2-user/nodejsapp
 pm2 start index.js
